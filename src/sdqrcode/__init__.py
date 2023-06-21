2c5472474aed29d7580efc83c9685e506e42c621
PhilSad <philippe.henri.saade@gmail.com>
2023-06-21 19:09:53 +0200

    updated import

--- src/sdqrcode/__init__.py
+++ src/sdqrcode/__init__.py
@@ -1,9 +1,1 @@
-from sdqrcode import (
-    Engine,
-    constants,
-    get_config,
-    generate_sd_qrcode,
-    Sdqrcode,
-    generate_qrcode_img,
-    generate_sd_qrcode_auto_api,
-)
+from sdqrcode.sdqrcode import *
