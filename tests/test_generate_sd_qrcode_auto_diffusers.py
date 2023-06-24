c0021b7364e6b7726942c359d93d8a64eb7c85b1
PhilSad <philippe.henri.saade@gmail.com>
2023-06-24 20:27:53 +0200

    fix

--- tests/test_generate_sd_qrcode_auto_diffusers.py
+++ tests/test_generate_sd_qrcode_auto_diffusers.py
@@ -13,14 +13,14 @@ class TestGenerateSDQRCode(unittest.TestCase):
         return super().setUp()
 
     def test_generate_sd_qrcode_diffusers(self):
-        sd_qr_code = init_and_generate_sd_qrcode(
+        sd_qr_code_img, _ = init_and_generate_sd_qrcode(
             config_name_or_path="./tests/test_configs/default_diffusers.yaml"
         )
 
-        self.assertIsNotNone(sd_qr_code)
+        self.assertIsNotNone(sd_qr_code_img)
 
-        for i, sd_qr_code_img in enumerate(sd_qr_code):
-            sd_qr_code_img.save(
+        for i, img in enumerate(sd_qr_code_img):
+            img.save(
                 f"./tests/imgs_test_results/test_generate_sd_qrcode_{i}_diffusers.png"
             )
 
